4234ffa28ce8b4b2557e4c87af548b5f633f11a9
abid7 <abidshahriar7@gmail.com>
2020-12-05 06:59:37 +0600

    started working on the skills section slide

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -3,7 +3,24 @@ import React from "react";
 export default function Skills() {
   return (
     <div className="section skills" id="skills">
-      <div className="section__wrapper">skills</div>
+      <div className="section__wrapper">
+        <div className="Skills__container">
+          <div className="swiper-container">
+            <div className="swiper-wrapper">
+              <div className="swiper-slide">
+                <div className="skill-box">front end</div>
+              </div>
+              <div className="swiper-slide">
+                <div className="skill-box">backend</div>
+              </div>
+              <div className="swiper-slide">
+                <div className="skill-box"> others</div>
+              </div>
+            </div>
+            <div className="swiper-pagination"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
