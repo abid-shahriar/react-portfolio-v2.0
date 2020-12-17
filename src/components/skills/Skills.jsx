770ced80d307027f744622dd98086bdc6d36177f
abid7 <abidshahriar7@gmail.com>
2020-12-17 10:20:52 +0600

    started working on skills section

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,21 +5,43 @@ export default function Skills() {
     <div className="section skills" id="skills">
       <div className="section__wrapper">
         <div className="Skills__container">
-          <div className="swiper-container">
-            <div className="swiper-wrapper">
-              <div className="swiper-slide">
-                <div className="skill-box">front end</div>
-              </div>
-              <div className="swiper-slide">
-                <div className="skill-box">backend</div>
-              </div>
-              <div className="swiper-slide">
-                <div className="skill-box"> others</div>
-              </div>
+          <div className="skill-box">
+            <h3>Frontend Skills</h3>
+            <div className="skill-box_skills">
+              <ul>
+                <li>HTML</li>
+                <li>CSS/SASS</li>
+                <li>JavaScript</li>
+                <li>ReactJS</li>
+              </ul>
             </div>
-            <div className="swiper-pagination"></div>
+          </div>
+
+          <div className="skill-box">
+            <h3>Backend Skills</h3>
+            <div className="skill-box_skills">
+              <ul>
+                <li>NodeJS</li>
+                <li>Express</li>
+                <li>Mongoose</li>
+                <li>MongoDB</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="skill-box">
+            <h3>Other Skills</h3>
+            <div className="skill-box_skills">
+              <ul>
+                <li>Git/Github</li>
+                <li>Redux</li>
+                <li>ContextApi</li>
+                <li>Capnel</li>
+              </ul>
+            </div>
           </div>
         </div>
+        <div className="swiper-pagination"></div>
       </div>
     </div>
   );
