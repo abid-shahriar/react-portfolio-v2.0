3507d8a9ed9ad7cf92fa136ddb830dbb18ba25b5
abid7 <abidshahriar7@gmail.com>
2020-12-18 17:03:30 +0600

    started working on the contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Contact() {
   return (
     <section className="section contact" id="contact">
-      <div className="section__wrapper">contact</div>
+      <div className="section__wrapper">
+        <div className="conatact__container"></div>
+      </div>
     </section>
   );
 }
