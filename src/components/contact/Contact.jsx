3a341fdaa480fdee779fe9525add66182cf78cdf
abid7 <abidshahriar7@gmail.com>
2020-12-18 18:37:44 +0600

    almost done with contact from

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -8,18 +8,28 @@ export default function Contact() {
           <form action="" className="contact__form">
             <div className="name">
               <label htmlFor="name">Your Name</label>
-              <input name="name" type="text" placeholder="example: john Doe" />
+              <input
+                required
+                name="name"
+                type="text"
+                placeholder="example: john Doe"
+              />
             </div>
             <div className="email">
               <label htmlFor="email">Your Email</label>
               <input
+                required
                 name="email"
                 type="text"
                 placeholder="example: johndoe@gmail.com"
               />
             </div>
             <div className="message">
-              <textarea name="message" placeholder="Your Message" />
+              <textarea required name="message" placeholder="Your Message" />
+            </div>
+
+            <div className="btn">
+              <button type="submit">Send Message</button>
             </div>
           </form>
         </div>
