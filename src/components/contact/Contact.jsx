339a5051c687644465ff74c479084e92475dadd9
abid7 <abidshahriar7@gmail.com>
2020-12-18 17:07:45 +0600

    working on contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,22 @@ export default function Contact() {
   return (
     <section className="section contact" id="contact">
       <div className="section__wrapper">
-        <div className="conatact__container"></div>
+        <div className="conatact__container">
+          <form action="" className="Contact__form">
+            <div className="name">
+              <label htmlFor="name">Name</label>
+              <input name="name" type="text" placeholder="Your Name Here" />
+            </div>
+            <div className="email">
+              <label htmlFor="email">Email</label>
+              <input name="email" type="text" placeholder="Your Email Here" />
+            </div>
+            <div className="message">
+              <label htmlFor="message">Name</label>
+              <input name="message" type="text" placeholder="Your Message" />
+            </div>
+          </form>
+        </div>
       </div>
     </section>
   );
