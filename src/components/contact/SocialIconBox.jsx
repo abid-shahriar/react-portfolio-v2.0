cca922e82fd8a4e1dd3a28a3cc2a8089707d4bb1
abid7 <abidshahriar7@gmail.com>
2020-12-20 02:13:48 +0600

    added target blank to social icons

--- src/components/contact/SocialIconBox.jsx
+++ src/components/contact/SocialIconBox.jsx
@@ -7,17 +7,29 @@ const SocialIconBox = () => {
   return (
     <div className="footer__social-icons">
       <div className="icon-box fb">
-        <a href="https://facebook.com/abid.sh.akash">
+        <a
+          href="https://facebook.com/abid.sh.akash"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={fb} alt="abid shahriar facebook" />
         </a>
       </div>
       <div className="icon-box insta">
-        <a href="https://instagram.com/find.abid">
+        <a
+          href="https://instagram.com/find.abid"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={insta} alt="abid shahriar istagram" />
         </a>
       </div>
       <div className="icon-box linkedin">
-        <a href="https://linkedin.com/abid.sh.akash">
+        <a
+          href="https://linkedin.com/abid.sh.akash"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={linkedin} alt="abid shahriar facebook" />
         </a>
       </div>
