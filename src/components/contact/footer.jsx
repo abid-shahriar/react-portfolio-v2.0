d7529f60b06b3923657eedd8c9e1eeffec3f3bf7
abid7 <abidshahriar7@gmail.com>
2020-12-19 12:30:23 +0600

    footer is almost done

--- src/components/contact/footer.jsx
+++ src/components/contact/footer.jsx
@@ -6,6 +6,14 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer__container">
         <SocialIconBox />
+
+        <div className="footer__message">
+          <p>Retro Style Portfolio - Built with React</p>
+          <p>by</p>
+          <p>
+            <a href="https://github.com/abid-shahriar">Abid Shahriar</a>
+          </p>
+        </div>
       </div>
     </footer>
   );
